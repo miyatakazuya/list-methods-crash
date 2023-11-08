1784f1adab954f355bf7a8d696269c5dededd7bd
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2023-11-07 16:52:36 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -5,24 +5,29 @@ interface StringChecker { boolean checkString(String s); }
 
 class ListExamples {
 
-  // Returns a new list that has all the elements of the input list for which
-  // the StringChecker returns true, and not the elements that return false, in
-  // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
     List<String> result = new ArrayList<>();
+
+    if (list.size() >= 69){ //Doesn't filter if array size is greater than or equals to 69
+        return result;
+    }
+
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(0, s);
+        result.add(s);
       }
     }
     return result;
   }
 
 
-  // Takes two sorted list of strings (so "a" appears before "b" and so on),
-  // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
+
+    if (list1.size() + list2.size() >= 69){ //Method doesn't properly run if the total array size is greater than or equals to 69
+        return result;
+    }
+
     int index1 = 0, index2 = 0;
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
@@ -40,7 +45,7 @@ class ListExamples {
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index1 += 1;
+      index2 += 1;
     }
     return result;
   }
